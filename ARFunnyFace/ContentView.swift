2307661a5257789eee99e6dfea61e368621e7758
Alexander Snitko <asyncamisu@gmail.com>
2022-06-19 11:10:11 +0300

    3 scenes added

--- ARFunnyFace/ContentView.swift
+++ ARFunnyFace/ContentView.swift
@@ -8,23 +8,61 @@
 import SwiftUI
 import RealityKit
 
+var arView: ARView!
+
 struct ContentView : View {
+    
+    @State var propId: Int = 0
+        
     var body: some View {
-        return ARViewContainer().edgesIgnoringSafeArea(.all)
+        return ZStack(alignment: .bottom) {
+            ARViewContainer(propId: $propId).edgesIgnoringSafeArea(.all)
+            HStack {
+                Spacer()
+                Button {
+                    self.propId = self.propId <= 0 ? 0 : self.propId - 1
+                } label: {
+                    Image("PreviousButton").clipShape(Circle())
+                }
+                Spacer()
+                Button {
+                     self.takeSnapShot()
+                } label: {
+                    Image("ShutterButton").clipShape(Circle())
+                }
+                Spacer()
+                Button {
+                    self.propId = self.propId >= 2 ? 2 : self.propId + 1
+                } label: {
+                    Image("NextButton").clipShape(Circle())
+                }
+                Spacer()
+
+            }
+        }
+    }
+    
+    func takeSnapShot() {
+        arView.snapshot(saveToHDR: false) { image in
+            let compressedImage = UIImage(data: (image?.pngData())!)
+            UIImageWriteToSavedPhotosAlbum(compressedImage!, nil, nil, nil)
+        }
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
     
+    @Binding var propId: Int
+        
     func makeUIView(context: Context) -> ARView {
         
-        let arView = ARView(frame: .zero)
+        arView = ARView(frame: .zero)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+//         Load the "Box" scene from the "Experience" Reality File
+//        let boxAnchor = try! Experience.loadBox()
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+//        arView.scene.anchors.append(boxAnchor)
         
         return arView
         
